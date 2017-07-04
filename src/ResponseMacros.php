f94b12f1e4a304fa3147d86a732fbab2362d04a1
Bob Krijnen <krijnenbob@live.nl>
2017-07-04 12:57:57 +0200

    message macro

--- src/ResponseMacros.php
+++ src/ResponseMacros.php
@@ -3,6 +3,7 @@
 namespace Appstract\ResponseMacros;
 
 use Illuminate\Support\Facades\Response;
+use Illuminate\Contracts\Routing\ResponseFactory;
 
 class ResponseMacros
 {
@@ -10,14 +11,18 @@ class ResponseMacros
     /**
      * Constructor.
      */
-    public function __construct()
+    public function __construct(ResponseFactory $factory)
     {
-        Response::macro('upper', function ($value) {
-            return Response::make(strtoupper($value));
-        });
+    	// response()->message('hello world!', 200);
 
-        Response::macro('lower', function ($value) {
-            return Response::strtolower(str)($value);
+        $factory->macro('message', function ($message, $status) use ($factory) {
+            
+            $customFormat = [
+                'message' => $message,
+                'status' => $status
+            ];
+
+            return $factory->make($customFormat);
         });
     }
 
