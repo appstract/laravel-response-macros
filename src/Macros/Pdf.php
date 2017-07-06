9a493acf2398b0bee549247dd32549fdc5c445a1
Gijs Jorissen <gizburdt@gmail.com>
2017-07-06 16:43:29 +0200

    pdf download

--- src/Macros/Pdf.php
+++ src/Macros/Pdf.php
@@ -2,24 +2,18 @@
 
 namespace Appstract\ResponseMacros\Macros;
 
-use Illuminate\Support\Facades\Response;
 use Appstract\ResponseMacros\ResponseMacroInterface;
 
 class Pdf implements ResponseMacroInterface
 {
     public function run($factory)
     {
-        $factory->macro('pdf', function ($pdf, $fileName) use ($factory) {
-            return $factory->make($pdf)->withHeaders([
-                'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'attachment; filename="'.$fileName.'"',
-            ]);
+        $factory->macro('pdf', function ($pdf, $fileName, $download = false) use ($factory) {
+            return $factory->make($pdf)->withHeaders(collect([
+                'Content-Type' => 'application/pdf'
+            ])->when($download, function($collection){
+                return $collection->put('Content-Disposition', 'attachment; filename="'.$fileName.'"');
+            })->toArray());
         });
     }
 }
-
-// // response()->pdf($pdf, ‘filename’)
-
-// response($invoice->pdf(), 200)
-//             ->header('Content-Type', 'application/pdf')
-//             ->header('Content-Disposition', 'attachment; filename="'. $invoice->pdfFileName.'"')
